92245f634e8f61b339137463ef3b54ce03ccdd19
Gabriel de Sousa Souto <gsousax2008@Hotmail.com>
2021-05-04 19:12:37 -0300

    Exception handling - update

--- src/main/java/com/gabriel/spring/service/UserService.java
+++ src/main/java/com/gabriel/spring/service/UserService.java
@@ -2,14 +2,19 @@ package com.gabriel.spring.service;
 
 import com.gabriel.spring.repositories.UserRepository;
 import com.gabriel.spring.service.exceptions.DatabaseException;
+import com.gabriel.spring.service.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+
+
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import com.gabriel.spring.entities.*;
 
@@ -47,9 +52,17 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
+        try {
+
         User entity = repository.getOne(id);
         updateData(entity,obj);
         return repository.save(entity);
+
+        } catch (EntityNotFoundException e) {
+
+            throw new ResourceNotFoundException(id);
+        }
+
 
 
         /*getOne apenas nao instancia outro user, 
